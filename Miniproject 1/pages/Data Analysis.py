b4a35a483351ea2812e653c81620a0b45a94aa3e
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-04-19 09:37:54 +0530

    Update Data Analysis.py

--- Miniproject 1/pages/Data Analysis.py
+++ Miniproject 1/pages/Data Analysis.py
@@ -10,7 +10,7 @@ from scipy.stats import ttest_ind,  f_oneway
 
 # absolute path to this file
 # FILE_DIR = os.path.dirname(os.path.abspath("C://Users//Mrudula Madhavan//Desktop//scifor//Project//pages//Data Analysis.py"))
-FILE_DIR = os.path.dirname(os.path.abspath("Miniproject 1//pages//Data Analysis.py"))
+FILE_DIR = os.path.dirname(os.path.abspath("Miniproject_1//pages//Data Analysis.py"))
 # absolute path to this file's root directory
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
 # absolute path of directory_of_interest
