4aa78e991b44f3d96c41f9aa6c250d70938916aa
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-04-19 10:14:46 +0530

    Update Predictor.py

--- Miniproject_1/pages/Predictor.py
+++ Miniproject_1/pages/Predictor.py
@@ -10,13 +10,13 @@ from sklearn.model_selection import train_test_split
 from matplotlib import image
 
 st.set_page_config(page_title="Bike Sharing Demand Predictor", page_icon="https://www.shutterstock.com/image-vector/bicycle-filled-outline-icons-vector-illustration-1772580485",
-                   layout="wide")
+                   layout="centered")
 
 #import model
-st.title(":red[Bike Sharing Demand Predictor]")
+st.title(":red[  Bike Sharing Demand Predictor  ]")
 
 #resources path
-#FILE_DIR1 = os.path.dirname(os.path.abspath("C://Users//Mrudula Madhavan//Desktop//scifor//Project//pages//Predictor.py"))
+
 FILE_DIR1 = os.path.dirname(os.path.abspath("Miniproject_1//pages//Predictor.py"))
 FILE_DIR = os.path.join(FILE_DIR1,os.pardir)
 dir_of_interest = os.path.join(FILE_DIR, "resources")
@@ -28,35 +28,33 @@ st.image(img)
 DATA_PATH = os.path.join(dir_of_interest, "data")
 
 #Load data
-DATA_PATH1=os.path.join(DATA_PATH, "bike_dataset.csv")
-df=pd.read_csv(DATA_PATH1)
-df1 = df.copy
+DATA_PATH1=os.path.join(DATA_PATH, "bike_demand_cleaned.csv")
+df=pd.read_csv(DATA_PATH1,encoding='latin')
+df1 = df.copy()
 
-xgb = pickle.load(open('Miniproject_1//xgb_model.pkl','rb'))
+# ['Temperature(°C)', 'Humidity(%)', 'Wind speed (m/s)','Solar Radiation (MJ/m2)', 'Visibility (10m)', 'Month', 'Weekday','Hour', 'Holiday', 'Seasons', 'Rented Bike Count']
 
-
-def prediction(season,month,weekday,hour,temperature,humidity,visibility,windspeed,solarrdn,rainfall,snowfall):
-    prediction = xgb.predict([[season,month,weekday,hour,temperature,humidity,visibility,windspeed,solarrdn,rainfall,snowfall]])
+def prediction(season,month,weekday,hour,holiday,temperature,humidity,visibility,windspeed,solarrdn,rainfall):
+    prediction = xgb.predict([[season,month,weekday,hour,holiday,temperature,humidity,visibility,windspeed,solarrdn]])
     print(prediction)
     return prediction
 
+# ------------------------------------------------------------------------------------------------------------
+st.subheader("Enter the location details :")
 
-
-
-st.subheader("Enter the Location details :")
-
-col1,col2 = st.columns(2)
+col1,col2,col3 = st.columns(3)
 with col1:  
     season = st.selectbox("Season", df["Seasons"].unique())
 with col2:  
-    month = st.selectbox("Month", df["Month"].unique())
-
-
-col3,col4 = st.columns(2)
+    month = st.selectbox("Month", ['January', 'February', 'March', 'April', 'May','June', 'July', 'August', 'September', 'October', 'November','December'])
 with col3:  
-    weekday = st.selectbox("Weekday", df["Weekday"].unique())
+    weekday = st.selectbox("Weekday", ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday'])
+
+col4,col5 = st.columns(2)
 with col4:  
-    hour = st.selectbox("Part of Day", df["Hour"].unique())
+    hour = st.selectbox("Time Shift", df["Hour"].unique())
+with col5:  
+  holiday = st.selectbox("Holiday", df["Holiday"].unique())
 
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
@@ -72,20 +70,13 @@ with col1:
 with col2:
     solarrdn = st.number_input("Solar Radiation (MJ/m2) ")
 
+bike_count = 0
 
-col3,col4 = st.columns(2)
-with col3:  
-    rainfall = st.number_input("Rainfall(mm) ")
-with col4:
-    snowfall = st.number_input("Snowfall (cm) ")
-
-bike_count = ''
-
+#---------------------------------------------------------------------------------------------------------------
 #Create dataframe using all these values
-sample=pd.DataFrame({"Seasons":[season],"Month":[month],"Weekday":[weekday],"Hour":[hour],
-                    "Temperature(°C)":float(temperature),"Humidity(%)":float(humidity),"Visibility(10m)":float(visibility),
-                    "Wind speed (m/s)":[windspeed],"Solar Radiation (MJ/m2)":[solarrdn],
-                    "Rainfall(mm)":[rainfall], "Snowfall (cm)":[snowfall]})
+sample=pd.DataFrame({"Seasons":[season],"Month":[month],"Weekday":[weekday],"Hour":[hour],"Holiday":["holiday"],
+                    "Temperature(°C)":float(temperature),"Humidity(%)":float(humidity),"Visibility (10m)":float(visibility),
+                    "Wind speed (m/s)":[windspeed],"Solar Radiation (MJ/m2)":[solarrdn]})
 
 #Function to change season to number
 def replace_season(season):    
@@ -97,8 +88,38 @@ def replace_season(season):
         return 3.0
     else:
         return 4.0
-df1['Seasons'] = df1['Seasons'].apply(replace_season)
+df['Seasons'] = df['Seasons'].apply(replace_season)
 sample['Seasons'] = sample['Seasons'].apply(replace_season)
+
+def month_name(month):
+    if month == 'January':
+        return 1.0
+    elif month ==  'February':
+        return 2.0
+    elif month == 'March':
+        return 3.0
+    elif month == 'April':
+        return 4.0
+    elif month == 'May':
+        return 5.0
+    elif month == 'June':
+        return 6.0
+    elif month == 'July':
+        return 7.0
+    elif month == 'August':
+        return 8.0
+    elif month == 'September':
+        return 9.0
+    elif month == 'October':
+        return 10.0
+    elif month == 'November':
+        return 11.0
+    else:
+        return 12.0
+
+df['Month'] = df['Month'].apply(month_name).astype('float64')
+sample['Month'] = sample['Month'].apply(month_name).astype('float64')
+
 
 def replace_weekday(weekday):
     if weekday =='Sunday':
@@ -115,43 +136,44 @@ def replace_weekday(weekday):
         return 6.0
     else:
         return 7.0
-df1['Weekday'] = df1['Weekday'].apply(replace_weekday).astype('float64')
+df['Weekday'] = df['Weekday'].apply(replace_weekday).astype('float64')
 sample['Weekday'] = sample['Weekday'].apply(replace_weekday).astype('float64')
 
 # Function to distribute hour
 def distribute_hour(hour):
-    if 17 <= hour <= 22:
-        return 4.0  # 'Evening'
-    elif 7 <= hour <= 10:
-        return  2.0 # 'Morning'
-    elif 11 <= hour <= 16:
-        return  3.0 #'Afternoon'
+    if hour == '12 AM - 4 AM':
+        return 0.0  # 
+    elif hour == '4 AM - 8 AM':
+        return 1.0 # 
+    elif hour == '8 AM - 12 PM':
+        return 2.0 #
+    elif hour == '12 PM - 4 PM':
+        return 3.0
+    elif hour == '4 PM - 8 PM':
+        return 4.0
+    elif hour == '8 PM - 12 AM':
+        return 5.0
     else:
-        return 1.0  #'Night'
+        pass
 
-# hour_map = {'Night': 1.0,"Morning":2.0,"Afternoon":3.0, "Evening":4.0}
-# Apply the hour function
-df1['Hour'] = df1['Hour'].apply(distribute_hour).astype('float64')         # .map(hour_map).astype('float64')
-sample['Hour'] = sample['Hour'].apply(distribute_hour).astype('float64')      # .map(hour_map).astype('float64')
 
-df1['Month'] = df1['Month'].astype('float64')
-sample['Month'] = sample['Month'].astype('float64')
+df['Hour'] = df['Hour'].apply(distribute_hour).astype('float64')
+sample['Hour'] = sample['Hour'].apply(distribute_hour).astype('float64')
 
-df1['Wind speed (m/s)'] = np.sqrt(df1['Wind speed (m/s)'])
+def replace_holiday(holiday):
+    if holiday =='No':
+        return 0.0
+    else:
+        return 1.0
+df['Holiday'] = df['Holiday'].apply(replace_holiday)
+sample['Holiday'] = sample['Holiday'].apply(replace_holiday)
+
+df['Wind speed (m/s)'] = np.sqrt(df['Wind speed (m/s)'])
 sample['Wind speed (m/s)'] = np.sqrt(sample['Wind speed (m/s)'])
-# Standardizing the required column
-df1['Temperature(°C)'] = StandardScaler().fit_transform(df1['Temperature(°C)'].values.reshape(-1, 1))
-sample['Temperature(°C)'] = StandardScaler().fit_transform(sample['Temperature(°C)'].values.reshape(-1, 1))
-df1['Humidity(%)'] = StandardScaler().fit_transform(df1['Humidity(%)'].values.reshape(-1, 1))
-sample['Humidity(%)'] = StandardScaler().fit_transform(sample['Humidity(%)'].values.reshape(-1, 1))
-
-# Normalizing the required column
-df1['Visibility (10m)'] = MinMaxScaler().fit_transform(df1['Visibility (10m)'].values.reshape(-1, 1))
-sample['Visibility (10m)'] = MinMaxScaler().fit_transform(sample['Visibility (10m)'].values.reshape(-1, 1))
 
 #Split data into X and y
-X=df1.drop('Rented Bike Count', axis=1).values
-y=df1['Rented Bike Count'].values
+X=df.drop('Rented Bike Count', axis=1).values
+y=df['Rented Bike Count'].values
 
 #Standarizing the features
 std=StandardScaler()
@@ -159,7 +181,7 @@ std_fit=std.fit(X)
 X=std_fit.transform(X)
 
 # Splitting data into 75:25 ratio
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 #Train the model
 xgb=XGBRegressor(learning_rate=0.15, n_estimators=50, max_leaves=0, random_state=42)
@@ -168,11 +190,12 @@ xgb.fit(X,y)
 #Standardize the features
 sample=sample.values
 sample=std_fit.transform(sample)
-
+st.write("")
 #Prediction
-if st.button('Predict Demand'):
-    price=xgb.predict(sample)
-    bike_cnt=bike_count[0].round(2)    
-    st.subheader(":blue[The Predicted Value for Bike Rentals :] :green[{}]".format("$ "+str(bike_cnt)))
+if st.button('Predict Demand',key = "<uniquevalueofsomesort>"):
+    bike_count=xgb.predict(sample)
+    bike_count=bike_count[0] 
+    st.subheader(":blue[Mean Count of Bike Rentals :] :green[{}]".format(str(round(abs(bike_count)))))
 else:
     pass
+    
