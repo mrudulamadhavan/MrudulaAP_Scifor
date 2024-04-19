61c5346ee8242681e74e2f985a154ceed879eeff
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-04-19 09:04:48 +0530

    Add files via upload

--- Miniproject 1/Home.py
+++ Miniproject 1/Home.py
@@ -4,11 +4,11 @@ from matplotlib import image
 import os
 
 #Title of the home page
-st.header(":blue[Bike Sharing Demand Predictor]")
+st.title(":blue[Bike Sharing Demand Predictor]")
 
 
 #Adding Image
-FILE_DIR = os.path.dirname(os.path.abspath('"C://Users//Mrudula Madhavan//Desktop//scifor//miniproject//Home.py"'))
+FILE_DIR = os.path.dirname(os.path.abspath('miniproject//Home.py'))
 dir_of_interest = os.path.join(FILE_DIR, "resources")
 IMAGE_PATH = os.path.join(dir_of_interest, "images")
 IMAGE_PATH1 = os.path.join(IMAGE_PATH, "homepage.jpeg")
