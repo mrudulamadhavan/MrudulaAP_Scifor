d1c02b0fb4af89e7360d7b9cff3dce42bc6b5546
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-04-19 11:03:03 +0530

    Update Home.py

--- MainProject/Home.py
+++ MainProject/Home.py
@@ -7,7 +7,7 @@ import os
 st.markdown("<h1 style='text-align: center; color: red;'>Fitness Buddy</h1>", unsafe_allow_html=True)
 
 # Adding Image
-FILE_DIR = os.path.dirname(os.path.abspath("C://Users//Mrudula Madhavan//Desktop//scifor//MainProject//Home.py"))
+FILE_DIR = os.path.dirname(os.path.abspath("MainProject//Home.py"))
 dir_of_interest = os.path.join(FILE_DIR, "resources", "images")
 IMAGE_PATH = os.path.join(dir_of_interest, "homepage.png")
 
