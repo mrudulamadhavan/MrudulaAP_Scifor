97e94c5437d8882249e282dd8b2709a68f4492a3
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-04-30 08:02:19 +0530

    Update About Dataset.py

--- MainProject/pages/About Dataset.py
+++ MainProject/pages/About Dataset.py
@@ -59,7 +59,7 @@ st.write('----------------------------------------------------------------------
 
 
 if (status == 'Overview of dataset'):    
-    st.dataframe(df.sample(4).reset_index(drop=True))
+    st.dataframe(df.sample(4).reset_index(drop=True).T)
 
 elif (status == 'Shape'):
     rows = df.count()[0]
